7b8644027e4ea500da9d4d6950ce3cf0d66614ab
Roberto Casadei <robyonrails@gmail.com>
2021-04-19 17:45:57 +0200

    OOP machinery + adjusted build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import de.aaschmid.gradle.plugins.cpd.Cpd
+
 plugins {
     // Apply the java plugin to add support for Java
     java
@@ -12,12 +14,20 @@ plugins {
      * The runnable jar will be found in build/libs/projectname-all.jar
      */
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    
-    
+
+    checkstyle
+    pmd
+    id("de.aaschmid.cpd")
+    id("com.github.spotbugs")
+    `build-dashboard`
 }
+
 
 repositories {
     mavenCentral()
+    flatDir {
+        dirs("lib")
+    }
 }
 
 val javaFXModules = listOf(
@@ -47,19 +57,28 @@ dependencies {
     }
 
     // JUnit API and testing engine
+    implementation("junit:junit:_")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
-    
-    testCompile("org.testfx:testfx-junit5:4.0.16-alpha")
-    
+
+    testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
+
     implementation("com.google.guava:guava:30.1-jre")
     implementation("com.google.code.gson:gson:2.8.6")
 
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
-    
+
     implementation("one.util:streamex:0.7.3")
-    
+
     implementation("org.reflections:reflections:0.9.12")
+
+    runtimeOnly("org.junit.vintage:junit-vintage-engine")
+    File("lib")
+        .takeIf { it.exists() }
+        ?.takeIf { it.isDirectory }
+        ?.listFiles()
+        ?.filter { it.extension == "jar" }
+        ?.forEach { implementation(files("lib/${it.name}")) }
 }
 
 tasks.withType<JavaCompile> {
@@ -67,6 +86,7 @@ tasks.withType<JavaCompile> {
 }
 
 tasks.withType<Test> {
+    ignoreFailures = true
     // Enables JUnit 5 Jupiter module
     useJUnitPlatform()
 }
@@ -87,4 +107,49 @@ application {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+spotbugs {
+    setEffort("max")
+    setReportLevel("low")
+    showProgress.set(true)
+    val excludeFile = File("${project.rootProject.projectDir}/config/spotbugs/excludes.xml")
+    if (excludeFile.exists()) {
+        excludeFilter.set(excludeFile)
+    }
+}
+
+tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
+    ignoreFailures = true
+    reports {
+        create("html") {
+            enabled = true
+        }
+    }
+}
+
+pmd {
+    ruleSets = listOf()
+    ruleSetConfig = resources.text.fromFile("${project.rootProject.projectDir}/config/pmd/pmd.xml")
+    isIgnoreFailures = true
+}
+
+cpd {
+    isIgnoreFailures = true
+}
+
+tasks.withType<Cpd> {
+    reports {
+        xml.setEnabled(false)
+        text.setEnabled(true)
+    }
+    language = "java"
+    minimumTokenCount = 50
+    ignoreFailures = true
+    source = sourceSets["main"].allJava
+}
+
+checkstyle {
+    isIgnoreFailures = true
 }
